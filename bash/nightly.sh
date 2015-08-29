8160577a9ab4233fea92093350f8b735c42ca84f
Matt Gonzales <gonzmat2@isu.edu>
2015-08-29 14:19:32 -0600

    nightly commit

--- bash/nightly.sh
+++ bash/nightly.sh
@@ -65,13 +65,15 @@
 	clear
 	echo "Pushing /home/goit/ to git..."
 	git --git-dir=/home/goit/.git --work-tree=/home/goit/ commit -m "nightly commit"
-	git --git-dir=/home/goit/.git --work-tree=/home/goit/ push origin master
+	git --git-dir=/home/goit/.git --work-tree=/home/goit/ push nightly master
 
 	### Scripts
 	clear
 	echo "Pushing /home/goit/scripts to git..."
 	git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts commit -m "nightly commit"
-	git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts push origin master
+	git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts push nightly master
+
+	echo ""
 
 # Test code
 #git status |grep "Changes not staged for commit:"
