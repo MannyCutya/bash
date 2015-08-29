3f70ba481a47ed883443a91e9d67d49b11941b26
Matt Gonzales <gonzmat2@isu.edu>
2015-08-29 14:35:55 -0600

    nightly commit

--- bash/nightly.sh
+++ bash/nightly.sh
@@ -1,95 +1,81 @@
 #!/bin/bash
 
-progressArray=(
-		-
-		\
-		\|
-		/
-	)
-
-function Progress() {
-
-	for i in ${progressArray[@]}; do
-		echo $i
-	done
-}
-
 ### Update and Upgrade
 
-	date | grep "Sat"
-	if [[ "$?" == "0" ]]; then
-	    sudo apt-get update
-	    sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-	else
-	    sudo apt-get update
-	    sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-	fi
+    date | grep "Sat"
+    if [[ "$?" == "0" ]]; then
+        sudo apt-get update
+        sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+    else
+        sudo apt-get update
+        sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+    fi
 
 ### Update ClamAV
 
-	freshclam
-	clamscan -ri /srv/samba/ >> /var/log/clamav.log
+    freshclam
+    clamscan -ri /srv/samba/ >> /var/log/clamav.log
 
 ### Git ~
 
-	changeArrayHome=(
-	            .mozilla
-	            .config/autostart
-	            nightly.sh
-	            start_vnc.sh
-	            .vnc
-	            .vimrc
-	            README.md
-	            )
+    changeArrayHome=(
+                .mozilla
+                .config/autostart
+                nightly.sh
+                start_vnc.sh
+                .vnc
+                .vimrc
+                README.md
+                )
 
-	for i in ${changeArrayHome[@]}; do
-	    git status |grep "$i"
-	    if [[ "$?" == "0" ]]; then
-	    	clear
-	        git --git-dir=/home/goit/.git --work-tree=/home/goit/ add $i
-	        sleep 1
-	    fi
-	done
+    for i in ${changeArrayHome[@]}; do
+        git status |grep "$i"
+        if [[ "$?" == "0" ]]; then
+            clear
+            git --git-dir=/home/goit/.git --work-tree=/home/goit/ add $i
+            sleep 1
+        fi
+    done
 
 ### Git ~/scripts
 
-	clear
+    clear
     echo "Copying scripts from /home/goit..."
     sleep 1
     cp /home/goit/*.sh /home/goit/scripts/bash/
 
-	changeArrayScripts=(
-	            bash/*
-	            desktop/*
-	            README.md
-	            )
+    changeArrayScripts=(
+                bash/*
+                desktop/*
+                README.md
+                )
 
-	for i in ${changeArrayScripts[@]}; do
-	    git status |grep "$i"
-	    if [[ "$?" == "0" ]]; then
-	    	clear
-	        git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts add $i
-	        sleep 1
-	    fi
-	done
+    for i in ${changeArrayScripts[@]}; do
+        git status |grep "$i"
+        if [[ "$?" == "0" ]]; then
+            clear
+            git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts add $i
+            sleep 1
+        fi
+    done
 
 ### Push repos
 
-	### Home
-	clear
-	echo "Pushing /home/goit/ to git..." Progress
-	sleep 3
-	git --git-dir=/home/goit/.git --work-tree=/home/goit/ commit -m "nightly commit"
-	git --git-dir=/home/goit/.git --work-tree=/home/goit/ push origin master
+    ### Home
+    clear
+    echo "Pushing /home/goit/ to git..." Progress
+    sleep 3
+    git --git-dir=/home/goit/.git --work-tree=/home/goit/ commit -m "nightly commit"
+    git --git-dir=/home/goit/.git --work-tree=/home/goit/ push origin master
 
-	### Scripts
-	clear
-	echo "Pushing /home/goit/scripts to git..."
-	sleep 1
-	git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts commit -m "nightly commit"
-	git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts push origin master
+    ### Scripts
+    clear
+    echo "Pushing /home/goit/scripts to git..."
+    sleep 1
+    git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts commit -m "nightly commit"
+    git --git-dir=/home/goit/scripts/.git --work-tree=/home/goit/scripts push origin master
 
-	echo ""
+    echo ""
 
 # Test code
 #git status |grep "Changes not staged for commit:"
